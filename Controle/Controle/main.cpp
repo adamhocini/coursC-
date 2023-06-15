a0992eddf9ab1099b9b791e7d7e7a3e129221496
Adam Hocini <adam.hocini.ah@icloud.com>
2023-06-15 17:12:20 +0200

    .

--- Controle/Controle/main.cpp
+++ Controle/Controle/main.cpp
@@ -22,6 +22,8 @@ int main() {
         // Définition des boxeurs pour le combat
         combat.setCoinBleu(boxer1);
         combat.setCoinRouge(boxer2);
+    
+
 
         // Accès aux boxeurs dans le combat
         Boxer* coinBleu = combat.getCoinBleu();
@@ -30,6 +32,21 @@ int main() {
         // Affichage des noms des boxeurs
         cout << "Boxeur du coin bleu : " << coinBleu->GetNom() << endl;
         cout << "Boxeur du coin rouge : " << coinRouge->GetNom() << endl;
+    
+    // Instanciation automatique de Boxer
+        Boxer boxeur_1("Box_1", 75.0);
+        cout << "Adresse de boxeur_1 : " << &boxeur_1 << std::endl;
+        cout << "Nom de boxeur_1 : " << boxeur_1.GetNom() << std::endl;
+        cout << "Poids de boxeur_1 : " << boxeur_1.GetPoids() << " kg" << std::endl;
+
+        // Instanciation dynamique de Boxer
+        Boxer* boxeur_2 = new Boxer("Box_2", 78.0);
+        cout << "Adresse de boxeur_2 : " << boxeur_2 << std::endl;
+        cout << "Nom de boxeur_2 : " << boxeur_2->GetNom() << std::endl;
+        cout << "Poids de boxeur_2 : " << boxeur_2->GetPoids() << " kg" << std::endl;
+
+        // Libération de la mémoire
+        delete boxeur_2;
 
         // Libération de la mémoire
         delete boxer1;
